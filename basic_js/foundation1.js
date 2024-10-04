f233749aa83e673b9550c7a0466c5bd2d454168d
An Vu Nguyen <it.avng@gmail.com>
2024-10-04 14:51:36 +0700

    archived the previous topic

---
+++ basic_js/foundation1.js
@@ -0,0 +1,25 @@
+//print anything onto the terminal
+console.log("hello world");
+
+//declare mutable variable with `let`
+let varname;
+
+varname = "john";
+
+console.log(varname);
+
+varname = "henry";
+console.log(varname);
+
+//declare immutable with const
+const c = "3m2";
+
+console.log(c);
+
+// this will produce error
+//c = "2m2";
+//console.log(c);
+
+// a message with a variable
+console.log("this is a message", c);
+
